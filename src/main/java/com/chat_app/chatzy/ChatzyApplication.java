5df9b9f64c77e86df03d6f63f2a53a64c6b3c50b
J-Herika <138695574+J-Herika@users.noreply.github.com>
2025-12-12 15:12:38 +0300

    project creation

---
+++ src/main/java/com/chat_app/chatzy/ChatzyApplication.java
@@ -0,0 +1,13 @@
+package com.chat_app.chatzy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatzyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatzyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chat_app/chatzy/ChatzyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chat_app.chatzy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatzyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
