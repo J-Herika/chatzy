ec1d04eda9baa966f29cf961c88e53070f6c8268
J-Herika <138695574+J-Herika@users.noreply.github.com>
2025-12-12 15:55:06 +0300

    create a user class.

---
+++ src/main/java/com/chat_app/chatzy/model/User.java
@@ -0,0 +1,24 @@
+package com.chat_app.chatzy.model;
+
+import jakarta.persistence.*;
+
+@Entity(name = "Users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    long id;
+
+    @Column(unique = true, nullable = false)
+    String username;
+
+    public User(){}
+
+    public User(String username){
+        this.username = username;
+    }
+
+    public String getUsername() {return this.username;}
+    public void setUsername(String username) {this.username = username;}
+
+}
