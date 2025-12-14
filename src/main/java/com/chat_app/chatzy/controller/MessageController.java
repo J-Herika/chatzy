e8003475a7b40e08a54aa82c944fbd67807237b9
J-Herika <138695574+J-Herika@users.noreply.github.com>
2025-12-14 13:09:25 +0300

    add the final keyword where needed.

--- src/main/java/com/chat_app/chatzy/controller/MessageController.java
+++ src/main/java/com/chat_app/chatzy/controller/MessageController.java
@@ -14,7 +14,7 @@ import java.util.List;
 @RestController
 public class MessageController {
 
-    private MessageService messageService;
+    private final MessageService messageService;
 
     public MessageController(MessageService messageService){
         this.messageService = messageService;
